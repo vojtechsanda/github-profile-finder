7f352090f9a85c5a8bcffb9e966ae7ae3ecaba94
Vojtěch Šanda <vojtech.sanda@gmail.com>
2023-12-11 16:35:48 +0100

    feat: Add basic base MUI theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './styles/theme.ts';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes/routes.tsx';
 
@@ -14,6 +16,9 @@ const router = createBrowserRouter(routes, {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    background: {
+      default: '#1d1d1d',
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#e3e3e3',
+    },
+    primary: {
+      main: '#41F367',
+      dark: '#00c734',
+      light: '#a5faac',
+    },
+    secondary: {
+      main: '#f341cc',
+      dark: '#e000b3',
+      light: '#fbb8e9',
+    },
+  },
+});
