e71100b3c7108495762c4cd0a1e94867c506c59b
Vojtěch Šanda <vojtech.sanda@gmail.com>
2023-12-12 12:34:36 +0100

    feat: Implement proper account loader

--- src/loaders/accountLoader.ts
+++ src/loaders/accountLoader.ts
@@ -1,15 +1,15 @@
 import { defer } from 'react-router-dom';
+import { GitHubApi } from './githubApi';
 
-function simulateApiRequest(time: number) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ testingName: 'test' });
-    }, time);
-  });
-}
+const api = new GitHubApi();
 
-export const accountLoader = () => {
+export const accountLoader = (props: unknown) => {
+  const params = (props as { params: { accountId: string } }).params;
+
   return defer({
-    account: simulateApiRequest(2000),
+    account: (async () => {
+      const accountResult = await api.getExtendedAccount(params.accountId);
+      return accountResult.unwrap();
+    })(),
   });
 };
--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,16 +1,16 @@
-import type { Account } from '@/types/Account';
+import type { Account, ExtendedAccount } from '@/types/Account';
 import { Suspense } from 'react';
 import { Await, useLoaderData } from 'react-router-dom';
 import Loading from './Loading';
 
 export default function Account() {
-  // TODO: Differentiate error and user not found
+  // TODO: Differentiate error and account not found
   const { account } = useLoaderData() as { account: Account };
 
   return (
     <Suspense fallback={<Loading />}>
       <Await resolve={account} errorElement={<div>Account not found</div>}>
-        {(_account: Account) => <div>Found account {_account.testingName}</div>}
+        {(_account: ExtendedAccount) => <div>Found account {_account.name}</div>}
       </Await>
     </Suspense>
   );
