9c0856b162e440fde174bcf8062ef289354c0b34
Vojtěch Šanda <vojtech.sanda@gmail.com>
2023-12-12 18:56:26 +0100

    feat: Add repository sorting based on most stars

--- src/components/account/AccountRepositories.tsx
+++ src/components/account/AccountRepositories.tsx
@@ -1,10 +1,19 @@
 import { ExtendedAccount } from '@/types/Account';
 import { ExpandableList, RepositoryListItem } from '..';
+import { useMemo } from 'react';
 
 export default function AccountRepositories(account: ExtendedAccount) {
+  const sortedRepositories = useMemo(
+    () =>
+      [...account.repositories].sort(
+        (a, b) => (b.stargazers_count ?? 0) - (a.stargazers_count ?? 0),
+      ),
+    [account.repositories],
+  );
+
   return (
     <ExpandableList
-      list={account.repositories.map((repository) => {
+      list={sortedRepositories.map((repository) => {
         return <RepositoryListItem {...repository} key={repository.id} />;
       })}
       title="Repositories"
